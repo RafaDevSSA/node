445832a8d9ee6b7e77023733bd36021bbd61dfc3
Rafael Santana de Brito <leafarsb@hotmail.com>
2020-04-04 14:12:17 -0300

    jwt]

--- mini_api_estudos/src/config.js
+++ mini_api_estudos/src/config.js
@@ -1,11 +1,11 @@
-global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef'
+global.SALT_KEY = ''
 global.EMAIL_TMPL = '<strong>{0}</strong>'
-global.SMSSID = 'AC8d061f4d7f0d9c5a76499a465db1109d';
-global.SMSTOKEN = '598686e0f1d9420817d3cad808c78c95';
+global.SMSSID = '';
+global.SMSTOKEN = '';
 
 
 module.exports = {
-    connectionString: 'mongodb+srv://rsb:29059415@rsb-lmf5d.azure.mongodb.net/test',
-    sendgridKey: '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',
+    connectionString: '',
+    sendgridKey: '',
     containerConnectionString: 'TBD'
 }
